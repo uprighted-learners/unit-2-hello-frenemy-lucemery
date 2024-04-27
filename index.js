670075e99e9378a7e5f2c46e230a287a6a40b932
lucemery <lucemery316@gmail.com>
2024-04-26 19:53:10 -0500

    update

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function greeter(name) {
   // Your code goes here
+  if (name == "Darth Vader" || name == "Thanos") {
+    console.log("Go away" + " " + name + "!");
+  } else {
+    console.log("Hello my good friend," + name)
+  }
 }
+
+greeter("Luc");
+
+greeter("Darth Vader")
+
